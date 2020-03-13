22f80818bc962fc5843d9bc122d2ce8621a109ca
Marcelo Bini <marcelobini@gmail.com>
2020-03-13 19:34:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-(1..100).each do |number|
-  fizz = number % 3 == 0
-  buzz = number % 5 == 0
+1.upto(100) do |i|
+str = 'fizz' if (i % 3).zero?
+str = str ? 'fizzbuzz' : 'buzz' if (i % 5).zero?
+str = i unless str
+puts str
 end
