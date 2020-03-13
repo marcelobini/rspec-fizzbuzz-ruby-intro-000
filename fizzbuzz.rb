06dc3009d61651c47204d52dca4734926333b2fd
Marcelo Bini <marcelobini@gmail.com>
2020-03-13 19:32:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(number)
-    divisibleBy3 = (number % 3 == 0)
-    divisibleBy5 = (number % 5 == 0)
-
-    case
-        when divisibleBy3 && divisibleBy5
-            puts "FizzBuzz"
-        when divisibleBy3
-            puts "Fizz"
-        when divisibleBy5
-            puts "Buzz"
-        else
-            puts number
-    end
+(1..100).each do |number|
+  fizz = number % 3 == 0
+  buzz = number % 5 == 0
+  print "Fizz" if fizz
+  print "Buzz" if buzz
+  print number if !fizz && !buzz
+  print "\n"
 end
-
-(1..100).each {|n| fizzbuzz n}
