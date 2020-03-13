d66cfedf9facf2d1676703abe93c04f95ae9073d
Marcelo Bini <marcelobini@gmail.com>
2020-03-13 19:42:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizz_buzz_2(max)
-  arr = []
-  (1..max).each do |n|
-    if (n % 3 == 0)
-      if (n % 5 == 0)
-        arr << "FizzBuzz"
-      else
-        arr << "Fizz"
-      end
-    elsif (n % 5 == 0)
-      arr << "Buzz"
-    else
-      arr << n
-    end
+def fizzbuzz?(num)
+  case
+  when num % 15 == 0 then "FizzBuzz"
+  when num % 3  == 0 then "Fizz"
+  when num % 5  == 0 then "Buzz"
+  else num
   end
-  return arr
 end
