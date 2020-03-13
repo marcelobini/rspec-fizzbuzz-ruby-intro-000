95aa16b41526180ba062920772c1f3f382588886
Marcelo Bini <marcelobini@gmail.com>
2020-03-13 19:46:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(num)
-  1.upto(num).each do |x|
-    if x%3 == 0 && x%5 == 0
-      p "fizzbuzz"
-    elsif x % 5 == 0
-      p "buzz"
-    elsif x % 3 == 0
-      p "fizz"
-    else
-      p x
-    end
-  end
+(1..100).each do |i|
+if i % 5 == 0 && i % 3 == 0
+puts 'FizzBuzz'
+elsif i % 5 == 0
+puts 'Buzz'
+elsif i % 3 == 0
+puts 'Fizz'
+else
+puts i
+end
+
 end
