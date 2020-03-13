94504d0487821a96f5f156e316c4ef2c7a5dd2c7
Marcelo Bini <marcelobini@gmail.com>
2020-03-13 19:48:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-(1..100).each do |i|
-if i % 5 == 0 && i % 3 == 0
-puts 'FizzBuzz'
-elsif i % 5 == 0
-puts 'Buzz'
-elsif i % 3 == 0
-puts 'Fizz'
-else
-puts i
-end
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+  end
+
+  if int % 5 == 0
+   return "Buzz"
+  end
+
+  if int % 3 == 0
+   return "Fizz"
+  end
 
 end
